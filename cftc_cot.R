9de601ec179e2b518ec432b485010bd95369385f
Bardur Thomsen <brur88@gmail.com>
2024-02-09 08:37:44 -0500

    Update cftc_cot.R

--- cftc_cot.R
+++ cftc_cot.R
@@ -1,15 +1,18 @@
+ASSET_SP500 <- 'E-MINI S&P 500 - CHICAGO MERCANTILE EXCHANGE'
+ASSET_NASDAQ <- 'NASDAQ MINI - CHICAGO MERCANTILE EXCHANGE'
+
 # CFTC Commitment of Traders Report
 url <- "https://www.cftc.gov/dea/newcot/deafut.txt"
 
 # download the header file here [https://www.cftc.gov/MarketReports/CommitmentsofTraders/HistoricalViewable/cotvariableslegacy.html]
 # ad copy the content to a csv file
-pathToHeaderFile <- '[path to header file]'
+pathToHeaderFile <- 'C:\\Users\\barth\\Desktop\\cot129.csv'
 
 # Path to store the study
-pathToStorage <-  '[add your path here]'
+pathToStorage <-  'C:\\Users\\barth\\Desktop\\cot-nas.csv'
 
 # What asset are we looking for
-assetToFind = 'NASDAQ MINI - CHICAGO MERCANTILE EXCHANGE'
+assetToFind <- ASSET_NASDAQ
 
 # Read data from CFTC into a table
 table <- read.table(url, sep = ",")
@@ -20,11 +23,11 @@ tableHeader <- read.csv(pathToHeaderFile, header = FALSE, sep = ",")
 # Set the table headers to the CFTC data
 colnames(table) <- tableHeader$V1
 
-summary <- subset(table, table$`Market and Exchange Names` == assetToFind)
+# Fetch the table row of the asset we are looking for
+assetTable <- subset(table, table$`Market and Exchange Names` == assetToFind)
 
-preview <- subset(summary, select = c("As of Date in Form YYMMDD", "Market and Exchange Names", "Commercial Positions-Long (All)","Commercial Positions-Short (All)"))
+# Fetch only the columns of interest for the asset table
+printPreview <- subset(summary, select = c("As of Date in Form YYMMDD", "Market and Exchange Names", "Commercial Positions-Long (All)","Commercial Positions-Short (All)"))
 
-write.table(preview, pathToStorage, sep = "|", col.names = FALSE, row.names = FALSE)
-
-
+write.table(printPreview, pathToStorage, sep = "|", col.names = FALSE, row.names = FALSE)
 
